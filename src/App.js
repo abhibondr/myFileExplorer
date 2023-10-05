249951c877e045ea188e2ca7688f3b9762feb7be
Abhishek Bondre <94267438+abhibondr@users.noreply.github.com>
2023-10-05 21:26:50 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import { useState } from "react";
+import "./styles.css";
+import explorer from "./data/FolderData";
+import Folder from "./components/Folder";
+import useTraverseTree from "./hooks/use-traverse-tree";
+
+export default function App() {
+  const [explorerData, setExplorerData] = useState(explorer);
+
+  const { insertNode } = useTraverseTree();
+
+  const handleInsertNode = (folderId, item, isFolder) => {
+    const finalTree = insertNode(explorerData, folderId, item, isFolder);
+    setExplorerData(finalTree);
+  };
+  return (
+    <div className="App">
+      <Folder explorer={explorerData} handleInsertNode={handleInsertNode} />
+    </div>
+  );
+}
